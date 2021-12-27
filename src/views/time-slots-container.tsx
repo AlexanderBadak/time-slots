be3fff7ec28d80c1e2647d788baf6c46f298f236
Alexander Badak <badak@sgs-media.de>
2021-12-27 11:54:08 +0100

    Add the Time Slots View

---
+++ src/views/time-slots-container.tsx
@@ -0,0 +1,25 @@
+import useApi from 'hooks/useApi'
+import React, { useEffect, useState } from 'react'
+import { TCompany } from 'types/api'
+import TimeSlotsView from './time-slots-view'
+
+const TimeSlotsContainer = () => {
+    const [companies, setCompanies] = useState<TCompany[]>([])
+
+    const api = useApi()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const companyData = await api.fetchCompanies()
+            setCompanies(companyData)
+        }
+        fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <TimeSlotsView companies={companies} />
+    )
+}
+
+export default TimeSlotsContainer
